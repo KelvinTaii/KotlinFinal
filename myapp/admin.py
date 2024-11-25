a35a4f19d64354c95653f7f238c714f082914056
kelvintaii <7kelvintai@gmail.com>
2024-11-25 15:25:02 +0300

    Initial Full Project Commit

---
+++ myapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from myapp.models import User,Product,Appointment,Member
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Appointment)
+admin.site.register(Member)
